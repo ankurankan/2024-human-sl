15f9e526a5ee6a812c8891b3a44c44c94f07e569
Ankur Ankan <ankurankan@gmail.com>
2024-04-19 16:01:05 +0200

    Updates

--- code/sl.R
+++ code/sl.R
@@ -2,8 +2,6 @@ source('test_dag.R')
 
 library(tidyverse)
 
-N_NODES <- 10
-
 oracle <- function(u, v, true_dag, accuracy=0.9){
 	if (runif(1) <= accuracy){
 		if (v %in% dagitty::children(true_dag, u)){
@@ -34,27 +32,51 @@ dag_to_adjmatrix <- function(daggity_obj) {
   return(ans_mat)
 }
 
-varnames <- sapply(1:N_NODES, function(v) {
-    paste0("x", v)
-})
+# Generates a random dag with n_nodes number of nodes with edge probability edge_prob.
+# Returns the SHD between the learned (using a greedy approach) and the true graph.
+run.sim <- function(n_nodes, edge_prob, oracle_acc){
+	varnames <- sapply(1:n_nodes, function(v) {
+	    paste0("x", v)
+	})
+	
+	dag <- pcalg::randomDAG(n=n_nodes, prob=edge_prob, lB=1, uB=1, V=varnames)
+	dag <- pcalg::pcalg2dagitty(as(dag, "matrix"), labels = varnames, type = "dag")
+	sim_data <- simulateSEM(dag, empirical=T)
+	
+	marg_cor <- compute_effects_marg(dag, sim_data)[, c('u', 'v', 'cor')]
+	marg_cor_thresh <- marg_cor[as.double(marg_cor$cor) > 0.05, ]
+	marg_cor_thresh_sorted <- marg_cor_thresh %>% arrange(desc(marg_cor_thresh$cor))
+	
+	learned_edges <- list()
+	for (i_row in 1:nrow(marg_cor_thresh_sorted)){
+		tryCatch({
+			oracle_edge <- oracle(marg_cor_thresh_sorted[i_row, 'u'], marg_cor_thresh_sorted[i_row, 'v'], true_dag=dag, accuracy=oracle_acc)
+		},
+		error=function(x){
+			browser()
+		})
 
-dag <- pcalg::randomDAG(n=N_NODES, prob=0.4, lB=1, uB=1, V=varnames)
-dag <- pcalg::pcalg2dagitty(as(dag, "matrix"), labels = varnames, type = "dag")
-sim_data <- simulateSEM(dag, empirical=T)
-
-marg_cor <- compute_effects_marg(dag, sim_data)[, c('u', 'v', 'cor')]
-marg_cor_thresh <- marg_cor[as.double(marg_cor$cor) > 0.05, ]
-marg_cor_thresh_sorted <- marg_cor_thresh %>% arrange(desc(marg_cor_thresh$cor))
-
-learned_edges <- list()
-for (i_row in 1:nrow(marg_cor_thresh_sorted)){
-	oracle_edge <- oracle(marg_cor_thresh_sorted[i_row, 'u'], marg_cor_thresh_sorted[i_row, 'v'], true_dag=dag)
-	learned_edges[[i_row]] <- oracle_edge
+		learned_edges[[i_row]] <- oracle_edge
+	}
+	learned_edges <- learned_edges %>% discard(is.null)
+	learned_dag <- dagitty(paste0("dag{", paste0(names(dag), collapse=" "), " ", paste0(lapply(learned_edges, function(x) paste0(x, collapse=" -> ")), collapse=" ") ,"}"))
+	
+	true_adj <- dag_to_adjmatrix(dag)
+	learned_adj <- dag_to_adjmatrix(learned_dag)
+	dist <- sum(abs(true_adj - learned_adj))
+	return(dist)
 }
-learned_edges <- learned_edges %>% discard(is.null)
-learned_dag <- dagitty(paste0("dag{", paste0(names(dag), collapse=" "), " ", paste0(lapply(learned_edges, function(x) paste0(x, collapse=" -> ")), collapse=" ") ,"}"))
 
-true_adj <- dag_to_adjmatrix(dag)
-learned_adj <- dag_to_adjmatrix(learned_dag)
-dist <- sum(abs(true_adj - learned_adj))
-print(dist)
+edge_probs <- seq(0.4, 0.9, 0.2)
+oracle_accs <- seq(0.1, 0.9, 0.2)
+n_nodes <- 5
+
+results <- c()
+for (edge_prob in edge_probs){
+	for (oracle_acc in oracle_accs){
+		shd <- replicate(100, run.sim(n_nodes=n_nodes, edge_prob=edge_prob, oracle_acc=oracle_acc))
+		mean_shd <- mean(shd)
+		results <- c(results, mean_shd)
+	}
+}
+run.sim(n_nodes=10, edge_prob=0.4, oracle_acc=0.1)
