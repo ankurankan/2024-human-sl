05e52da08d8dc8dcec950b221204de6b34865080
Ankur Ankan <ankurankan@gmail.com>
2024-04-22 07:18:32 +0200

    Reduce number of lines

--- code/make_plots.R
+++ code/make_plots.R
@@ -6,6 +6,8 @@ make_shd_plot <- function(filename, plot_filename){
 	d <- read.csv(filename, row.names=1)
 	d$edge_prob <- sapply(d$edge_prob, as.character)
 
+	d <- d %>% filter(edge_prob %in% c("0.1", "0.5", "0.9"))
+
 	d_long <- rbind(d, d)
 	d_long[(nrow(d)+1):nrow(d_long), 'mean_shd_human'] <- d[, 'mean_shd_hc']
 	d_long[(nrow(d)+1):nrow(d_long), 'std_error_human'] <- d[, 'std_error_hc']
@@ -16,7 +18,7 @@ make_shd_plot <- function(filename, plot_filename){
 	p <- ggplot(d_long, aes(x=oracle_acc, y=mean_shd, color=edge_prob, shape=alg, group=interaction(edge_prob, alg))) + 
 		geom_line() +
 		geom_point() +
-		geom_ribbon(aes(ymin=mean_shd - std_error, ymax=mean_shd + std_error, color=edge), alpha=.2, linetype=0, show.legend=F) + 
+		geom_ribbon(aes(ymin=mean_shd - std_error, ymax=mean_shd + std_error, color=edge_prob), alpha=.2, linetype=0, show.legend=F) + 
       		theme_minimal(base_size = 8) + 
 		theme(legend.position='top') +
 		labs(x = "Oracle Accuracy") +
