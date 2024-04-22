2b7d965af8e37e17a2c8b4072f526ad0d1e52713
Ankur Ankan <ankurankan@gmail.com>
2024-04-22 09:15:36 +0200

    Updates

--- code/test_dag.R
+++ code/test_dag.R
@@ -115,13 +115,19 @@ dag3 <- dagitty("dag{X -> M -> Y X -> Y}")
 dag4 <- dagitty("dag{ A -> X -> Z <- Y A -> Y }")
 
 # DAG 5:
-dag5 <- dagitty("dag{ x1 -> a -> b -> x2 x1 -> x2}")
+dag5 <- dagitty("dag{ x1 <- a -> b -> x2 x1 -> x2}")
 
-# print(test_dag(true_dag=dag2, dag=dagitty('dag{X M -> Y X -> Y}'), effect_type='marg'))
-# print(test_dag(true_dag=dag2, dag=dagitty('dag{X M -> Y X -> Y}'), effect_type='cond'))
+# DAG 6:
+dag6 <- dagitty("dag{ x3 -> x -> {x1 x2} -> y }")
+
+# print(test_dag(true_dag=dag2, dag=dagitty('dag{X -> M -> Y}'), effect_type='marg'))
+# print(test_dag(true_dag=dag2, dag=dagitty('dag{X -> M -> Y}'), effect_type='cond'))
 
 # print(test_dag(true_dag=dag3, dag=dagitty('dag{X -> M -> Y }'), effect_type='marg'))
 # print(test_dag(true_dag=dag3, dag=dagitty('dag{X -> M -> Y }'), effect_type='cond'))
 
-# print(test_dag(true_dag=dag5, dag=dagitty('dag{ x1 -> a b -> x2 x1 -> b}'), effect_type='marg'))
-# print(test_dag(true_dag=dag5, dag=dagitty('dag{ x1 -> a b -> x2 x1 -> b}'), effect_type='cond'))
+# print(test_dag(true_dag=dag5, dag=dagitty('dag{ x1 <- a b -> x2}'), effect_type='marg'))
+# print(test_dag(true_dag=dag5, dag=dagitty('dag{ x1 <- a b -> x2}'), effect_type='cond'))
+
+# print(test_dag(true_dag=dag6, dag=dagitty('dag{ x3 -> x -> {x1 x2} -> y}'), effect_type='marg'))
+# print(test_dag(true_dag=dag6, dag=dagitty('dag{ x3 -> x -> {x1 x2} -> y}'), effect_type='cond'))
