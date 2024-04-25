c58e13062a2f0fbfe328d51d2f3ec6cf3c320e63
Ankur Ankan <ankurankan@gmail.com>
2024-04-25 23:29:39 +0200

    Updates

--- code/make_plots.R
+++ code/make_plots.R
@@ -17,12 +17,12 @@ make_shd_plot <- function(filename, plot_filename){
 	d_human <- d %>% select(oracle_acc, edge_prob, human_shd_mean:human_sid_sd) %>% add_column(alg='human')
 	colnames(d_human) <- col_names
 
-	d_long <- rbind(d_hc, d_pc, d_human)
+	d_long_shd <- rbind(d_hc, d_human)
 
-	p_shd <- ggplot(d_long, aes(x=oracle_acc, y=shd_mean, color=edge_prob, shape=alg, group=interaction(edge_prob, alg))) + 
+	p_shd <- ggplot(d_long_shd, aes(x=oracle_acc, y=shd_mean, color=edge_prob, shape=alg, group=interaction(edge_prob, alg))) + 
 		geom_line() +
 		geom_point() +
-		geom_ribbon(aes(ymin=shd_mean - shd_sd, ymax=shd_mean + shd_sd, color=edge_prob), alpha=.2, linetype=0, show.legend=F) + 
+		geom_ribbon(aes(ymin=shd_mean - shd_sd, ymax=shd_mean + shd_sd, fill=edge_prob), alpha=.2, linetype=0, show.legend=F) + 
       		theme_minimal(base_size = 8) + 
 		theme(legend.position='top') +
 		labs(x = "Oracle Accuracy") +
@@ -32,16 +32,22 @@ make_shd_plot <- function(filename, plot_filename){
 
 	ggsave("plots/shd.pdf", p_shd, units='in')
 
-	p_sid <- ggplot(d_long, aes(x=oracle_acc, y=sid_mean, color=edge_prob, shape=alg, group=interaction(edge_prob, alg))) + 
+	# Take the best sid score for PC.
+	d_pc$sid_mean <- d_pc$shd_mean
+	d_pc$sid_sd <- d_pc$shd_sd
+
+	d_long_sid <- rbind(d_hc, d_pc, d_human)
+
+	p_sid <- ggplot(d_long_sid, aes(x=oracle_acc, y=sid_mean, color=edge_prob, shape=alg, group=interaction(edge_prob, alg))) + 
 		geom_line() +
 		geom_point() +
-		geom_ribbon(aes(ymin=sid_mean - sid_sd, ymax=sid_mean + sid_sd, color=edge_prob), alpha=.2, linetype=0, show.legend=F) + 
+		geom_ribbon(aes(ymin=sid_mean - sid_sd, ymax=sid_mean + sid_sd, fill=edge_prob), alpha=.2, linetype=0, show.legend=F) + 
       		theme_minimal(base_size = 8) + 
 		theme(legend.position='top') +
 		labs(x = "Oracle Accuracy") +
 		labs(y = "Mean SHD") + 
 		labs(color = "Edge Probability") +
-		ylim(0, 85)
+		ylim(0, 80)
 
 	ggsave("plots/sid.pdf", p_sid, units='in')
 }
