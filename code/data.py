d6ed106edc4a944ca871eb25f5d97ec85abae1f3
Ankur Ankan <ankurankan@gmail.com>
2024-11-20 16:42:34 +0100

    Sepearate data preprocessing

---
+++ code/data.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+
+def get_adult_df():
+    df = pd.read_csv("adult_proc.csv", index_col=0)
+    df.Age = pd.Categorical(
+        df.Age,
+        categories=["<21", "21-30", "31-40", "41-50", "51-60", "61-70", ">70"],
+        ordered=True,
+    )
+    df.Education = pd.Categorical(
+        df.Education,
+        categories=[
+            "Preschool",
+            "1st-4th",
+            "5th-6th",
+            "7th-8th",
+            "9th",
+            "10th",
+            "11th",
+            "12th",
+            "HS-grad",
+            "Some-college",
+            "Assoc-voc",
+            "Assoc-acdm",
+            "Bachelors",
+            "Prof-school",
+            "Masters",
+            "Doctorate",
+        ],
+        ordered=True,
+    )
+    df.HoursPerWeek = pd.Categorical(
+        df.HoursPerWeek, categories=["<=20", "21-30", "31-40", ">40"], ordered=True
+    )
+    df.Workclass = pd.Categorical(df.Workclass, ordered=False)
+    df.MaritalStatus = pd.Categorical(df.MaritalStatus, ordered=False)
+    df.Occupation = pd.Categorical(df.Occupation, ordered=False)
+    df.Relationship = pd.Categorical(df.Relationship, ordered=False)
+    df.Race = pd.Categorical(df.Race, ordered=False)
+    df.Sex = pd.Categorical(df.Sex, ordered=False)
+    df.NativeCountry = pd.Categorical(df.NativeCountry, ordered=False)
+    df.Income = pd.Categorical(df.Income, ordered=False)
+    return (df)
--- code/figure4.py
+++ code/figure4.py
@@ -5,48 +5,8 @@ from itertools import combinations
 
 from pgmpy.estimators import ExpertInLoop
 
+from data import get_adult_df
 
-def preprocess_data():
-    df = pd.read_csv("adult_proc.csv", index_col=0)
-    df.Age = pd.Categorical(
-        df.Age,
-        categories=["<21", "21-30", "31-40", "41-50", "51-60", "61-70", ">70"],
-        ordered=True,
-    )
-    df.Education = pd.Categorical(
-        df.Education,
-        categories=[
-            "Preschool",
-            "1st-4th",
-            "5th-6th",
-            "7th-8th",
-            "9th",
-            "10th",
-            "11th",
-            "12th",
-            "HS-grad",
-            "Some-college",
-            "Assoc-voc",
-            "Assoc-acdm",
-            "Bachelors",
-            "Prof-school",
-            "Masters",
-            "Doctorate",
-        ],
-        ordered=True,
-    )
-    df.HoursPerWeek = pd.Categorical(
-        df.HoursPerWeek, categories=["<=20", "21-30", "31-40", ">40"], ordered=True
-    )
-    df.Workclass = pd.Categorical(df.Workclass, ordered=False)
-    df.MaritalStatus = pd.Categorical(df.MaritalStatus, ordered=False)
-    df.Occupation = pd.Categorical(df.Occupation, ordered=False)
-    df.Relationship = pd.Categorical(df.Relationship, ordered=False)
-    df.Race = pd.Categorical(df.Race, ordered=False)
-    df.Sex = pd.Categorical(df.Sex, ordered=False)
-    df.NativeCountry = pd.Categorical(df.NativeCountry, ordered=False)
-    df.Income = pd.Categorical(df.Income, ordered=False)
-    return (df)
 
 descriptions = {'Age': 'The age of a person',
                 'Workclass': 'The workplace where the person is employed such as Private industry, or self employed',
@@ -62,7 +22,7 @@ descriptions = {'Age': 'The age of a person',
                 }
 
 
-est = ExpertInLoop(preprocess_data())
+est = ExpertInLoop(get_adult_df())
 dag = est.estimate(
         pval_threshold=0.05,
         effect_size_threshold=0.05,
