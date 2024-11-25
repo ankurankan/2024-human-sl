c7f96cfedf43f5139132b6e6abd236fef3f727af
Ankur Ankan <ankurankan@gmail.com>
2024-11-25 10:06:41 +0000

    Minor fixes

--- code/fig3/sl_ges.py
+++ code/fig3/sl_ges.py
@@ -6,10 +6,11 @@ import networkx as nx
 from pgmpy.estimators import GES
 
 filename = sys.argv[1]
+# filename = "temp/8gu4Q.csv"
 df = pd.read_csv(filename)
 for col in df.columns:
-    if df.loc[:, col].dtype == 'int64':
-        df.loc[:, col] = df.loc[:, col].astype('str')
+    if df[col].dtype == 'int64':
+        df[col] = df[col].astype('category')
 
 est = GES(df)
 dag = est.estimate(scoring_method='cond-gauss')
