a930a3c19fa56c2d27afe48168a587a400fe2eda
Ankur Ankan <ankurankan@gmail.com>
2024-11-26 19:43:28 +0000

    Updates

--- code/fig1/adult_sl.R
+++ code/fig1/adult_sl.R
@@ -317,6 +317,7 @@ d$EducationNum <- NULL
 d$CapitalGain <- NULL
 d$CaptialLoss <- NULL
 
+# Categorical
 d$Workclass <- factor(d$Workclass, ordered = F)
 d$MaritalStatus <- factor(d$MaritalStatus, ordered = F)
 d$Occupation <- factor(d$Occupation, ordered = F)
@@ -326,11 +327,14 @@ d$Sex <- factor(d$Sex, ordered = F)
 d$NativeCountry <- factor(d$NativeCountry, ordered = F)
 d$Income <- factor(d$Income, ordered = F)
 
+# Continuous
 d$Age <- as.double(d$Age)
 d$HoursPerWeek <- as.double(d$HoursPerWeek)
 
+# Ordinal
 education_levels <- c("Preschool", "1st-4th", "5th-6th", "7th-8th", "9th", "10th", "11th", "12th", "HS-grad", "Some-college", "Assoc-voc", "Assoc-acdm", "Bachelors", "Masters", "Prof-school", "Doctorate")
 d$Education <- factor(d$Education, levels = education_levels, ordered = T)
+
 d <- d[complete.cases(d), ]
 d <- d[1:1000, ]
 
@@ -363,3 +367,5 @@ pc.skel <- pcalg::pc(
 		numCores = 8,
 		verbose = F
 		)
+
+adj_mat <- pcalg::dag2cpdag(as(pc.skel, "amat")) # Use this to get the final model.
