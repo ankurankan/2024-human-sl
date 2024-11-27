c190ebf397de56ec90a1c89dcbd640d5d9341bf5
Ankur Ankan <ankurankan@gmail.com>
2024-11-27 22:52:15 +0100

    Fix plot

--- code/fig5/make_plots.R
+++ code/fig5/make_plots.R
@@ -3,20 +3,24 @@ library(ggplot2)
 
 d_expert <- read.csv('results/unexplained_effect.txt', header=F)
 d_expert <- t(d_expert)
-colnames(d_expert) <- c('expert_pillai', 'expert_ll')
+colnames(d_expert) <- c('pillai', 'll')
 rownames(d_expert) <- seq(1, nrow(d_expert))
 d_expert <- as.data.frame(d_expert)
 d_expert <- cbind(d_expert, x=seq(1, nrow(d_expert)))
+d_expert$algo <- 'Expert'
 
 d_ges <- read.csv('results/ges_unexplained_effect.txt', header=F)
 d_ges <- as.data.frame(t(d_ges))
-colnames(d_ges) <- c('ges_pillai', 'ges_ll')
+colnames(d_ges) <- c('pillai', 'll')
 rownames(d_ges) <- seq(1, nrow(d_ges))
 d_ges <- cbind(d_ges, x=seq(1, nrow(d_ges)))
+d_ges$algo <- 'GES'
 
-p <- ggplot(d_expert, aes(x=x, y=pillai)) +
-	geom_line(alpha=0.6, show.legend=F) +
-	geom_point() +
+d_results = rbind(d_expert, d_ges)
+
+p <- ggplot(d_results, aes(x=x, y=pillai, color=algo)) +
+	geom_line(alpha=0.6) +
+	geom_point(alpha=0.6, show.legend=F) +
 	theme_minimal(base_size=8) +
 	labs(x="Iteration No.") +
 	labs(y="Unexplained Effect")
