0fc86cc1a5ce47a52fdf41013afbcf7e7e81749e
Ankur Ankan <ankurankan@gmail.com>
2024-11-28 19:17:28 +0100

    Adds code for generating the plot

--- code/fig5/llm.py
+++ code/fig5/llm.py
@@ -155,4 +155,3 @@ descriptions = {'Age': 'The age of a person',
 dag = simulate_human(preprocess_data(), descriptions)
 with open('llm_adult.txt', 'w') as f:
     f.write(str(list(dag.edges())))
-dag.to_graphviz().draw('llm_adult.png', prog='dot')
---
+++ code/fig5/make_plot.py
@@ -0,0 +1,7 @@
+from pgmpy.models import BayesianNetwork
+
+with open('adult_llm.txt', 'r') as f:
+    edge_list = eval(f.readline())
+
+dag = BayesianNetwork(edge_list)
+dag.to_graphviz().draw('llm_adult.png', prog='dot')
