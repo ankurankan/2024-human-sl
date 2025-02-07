41a9de4d39d55d26f66cfa1a6e0723efef34c4d3
Ankur Ankan <ankurankan@gmail.com>
2025-02-08 00:00:39 +0100

    Fixes RMSEA

--- web/api.R
+++ web/api.R
@@ -117,11 +117,13 @@ rmsea <- function( dag ){
 	
 			if (!(nodes[i] %in% pa_j) & !(nodes[j] %in% pa_i)){
 				tst <- dagitty::ciTest(X=nodes[i], Y=nodes[j], Z=union(pa_i, pa_j), dataset, type="cis.pillai")
-				pvalues <- c(pvalues, tst[,"p.value"])
+				pvalues <- c(pvalues, max(tst[,"p.value"], 1e-20))
 			}
 		}
 	}
+
 	fisherc <- -2 * sum(log(pvalues))
 	# return (pchisq(fisherc, 2*length(pvalues), lower.tail=F))
-	return(sqrt( (fisherc**2 - 2*length(pvalues)) / (2*length(pvalues)) / nrow(dataset) ))
+	print(pvalues)
+	return(sqrt( max(fisherc**2 - 2*length(pvalues), 0) / (2*length(pvalues) * nrow(dataset) )))
 }
