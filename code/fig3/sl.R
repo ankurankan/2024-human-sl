0f400873140f01663ef9fb27908549cf4eaae558
Ankur Ankan <ankurankan@gmail.com>
2025-05-08 09:50:24 +0200

    Adds another data point of 0 accuracy for the oracle.

--- code/fig3/sl.R
+++ code/fig3/sl.R
@@ -270,7 +270,7 @@ run_sim <- function(R, n_nodes, edge_probs, oracle_accs){
 }
 
 
-edge_probs <- seq(0.1, 0.9, 0.1)
+edge_probs <- c(0, seq(0.1, 0.9, 0.1))
 oracle_accs <- seq(0.1, 0.9, 0.2)
 n_nodes <- 10
 R <- 30
