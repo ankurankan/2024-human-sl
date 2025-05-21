9e9515c2614f5e3632b49429ba894feca6cfc19a
Ankur Ankan <ankurankan@gmail.com>
2025-05-21 12:49:21 +0200

    Updates web tool

--- web/README.md
+++ web/README.md
@@ -1,8 +1,15 @@
-Run the API server using:
+Steps to run the app:
 
-```R
-library(plumber)
-
-pr("plumber.R") %>%
-  pr_run(port=8000)
+1. Run the backend API server using:
+```bash
+Rscript run_server.R
 ```
+
+2. Open `index.html` in the browser.
+
+The app should be ready to use.
+
+Notes:
+
+1. The current implementation by default only works for continuous variables.
+2. To use it on mixture of datasets, a preprocessing function called `preprocess_dataset` needs to be added to `api.R`. An example function exists for the adult income dataset.
--- web/api.R
+++ web/api.R
@@ -1,30 +1,33 @@
 library(jsonlite)
 
-preprocess_adult <- function(d){
-	print(d)
-	d$X <- NULL
-	d$Fnlwgt <- NULL
-	d$EducationNum <- NULL
-	d$CapitalGain <- NULL
-	d$CaptialLoss <- NULL
+# preprocess_dataset <- function(d){
+# 	d$X <- NULL
+# 	d$Fnlwgt <- NULL
+# 	d$EducationNum <- NULL
+# 	d$CapitalGain <- NULL
+# 	d$CaptialLoss <- NULL
+# 
+# 	d$Workclass <- factor(d$Workclass, ordered=F)
+# 	d$MaritalStatus <- factor(d$MaritalStatus, ordered=F)
+# 	d$Occupation <- factor(d$Occupation, ordered=F)
+# 	d$Relationship <- factor(d$Relationship, ordered=F)
+# 	d$Race <- factor(d$Race, ordered=F)
+# 	d$Sex <- factor(d$Sex, ordered=F)
+# 	d$NativeCountry <- factor(d$NativeCountry, ordered=F)
+# 	d$Income <- factor(d$Income, ordered=F)
+# 	
+# 	d$Age <- as.double(d$Age)
+# 	d$HoursPerWeek <- as.double(d$HoursPerWeek)
+# 	
+# 	education_levels = c( "Preschool", "1st-4th", "5th-6th", "7th-8th", "9th", "10th", "11th","12th", "HS-grad", "Some-college", "Assoc-voc", "Assoc-acdm", "Bachelors", "Masters", "Prof-school", "Doctorate" )
+# 	d$Education <- factor(d$Education, levels=education_levels, ordered=T)
+# 	d <- d[complete.cases(d), ]
+# 	d <- d[1:1000, ]
+# 	
+# 	return(d)
+# }
 
-	d$Workclass <- factor(d$Workclass, ordered=F)
-	d$MaritalStatus <- factor(d$MaritalStatus, ordered=F)
-	d$Occupation <- factor(d$Occupation, ordered=F)
-	d$Relationship <- factor(d$Relationship, ordered=F)
-	d$Race <- factor(d$Race, ordered=F)
-	d$Sex <- factor(d$Sex, ordered=F)
-	d$NativeCountry <- factor(d$NativeCountry, ordered=F)
-	d$Income <- factor(d$Income, ordered=F)
-	
-	d$Age <- as.double(d$Age)
-	d$HoursPerWeek <- as.double(d$HoursPerWeek)
-	
-	education_levels = c( "Preschool", "1st-4th", "5th-6th", "7th-8th", "9th", "10th", "11th","12th", "HS-grad", "Some-college", "Assoc-voc", "Assoc-acdm", "Bachelors", "Masters", "Prof-school", "Doctorate" )
-	d$Education <- factor(d$Education, levels=education_levels, ordered=T)
-	d <- d[complete.cases(d), ]
-	d <- d[1:1000, ]
-	
+preprocess_dataset <- function(d){
 	return(d)
 }
 
@@ -73,7 +76,7 @@ run_citests <- function( req, res, dag, threshold, pval ){
 		g <- dagitty::dagitty(dag)
 		r <- c()
 		nn <- names(g)
-		dataset <- preprocess_adult(datasets[[1]])
+		dataset <- preprocess_dataset(datasets[[1]])
 		for( n1i in seq(1,length(nn)-1,by=1) ){
 			n1 <- nn[n1i]
 			p1 <- dagitty::parents(g, n1)
@@ -110,7 +113,7 @@ rmsea <- function( dag ){
 	g <- dagitty::dagitty(dag)
 	pvalues <- c()
 	nodes <- names(g)
-	dataset <- preprocess_adult(datasets[[1]])
+	dataset <- preprocess_dataset(datasets[[1]])
 	for (i in 1:(length(nodes)-1)){
 		for (j in (i+1):length(nodes)){
 			pa_i <- dagitty::parents(g, nodes[i])
